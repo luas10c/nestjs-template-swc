b9a4d400d79675b167e8c9c8c47fe563e4761b9a
Luciano Alves <luas10c@gmail.com>
2023-01-26 16:02:31 -0300

    chore: first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module.js";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(4000, "0.0.0.0");
+}
+
+bootstrap();
